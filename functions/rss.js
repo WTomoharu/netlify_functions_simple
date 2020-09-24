ac7190bc9e93c4f82419999565ea439cb8ca9a49
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-24 19:16:00 +0900

    requireを修正

--- functions/rss.js
+++ functions/rss.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-    const axios = require('axios');
+    const axios = require('axios')
     const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data})
     return {
       statusCode: 200,
