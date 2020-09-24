df6df65c74b7c60b251bbd0e934b265f45613272
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-24 18:56:47 +0900

    rss.jsのimport axiosを追記

--- functions/rss.js
+++ functions/rss.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 exports.handler = async (event, context) => {
     const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data})
     return {
