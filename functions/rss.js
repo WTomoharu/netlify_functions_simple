45839df8c0c99b28c1eee338f8b412aaad019fb7
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-24 19:17:12 +0900

    コードフォーマッターを掛けた

--- functions/rss.js
+++ functions/rss.js
@@ -1,8 +1,8 @@
 exports.handler = async (event, context) => {
-    const axios = require('axios')
-    const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data})
-    return {
-      statusCode: 200,
-      body: "Hello, RSS"
-    }
+  const axios = require('axios')
+  const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data })
+  return {
+    statusCode: 200,
+    body: "Hello, RSS"
   }
+}
