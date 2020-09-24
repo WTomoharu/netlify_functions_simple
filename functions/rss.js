398c4d75f66770e325e747387adf189154aee1fe
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-24 19:19:36 +0900

    問題部分をコメントアウト化

--- functions/rss.js
+++ functions/rss.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
-  const axios = require('axios')
-  const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data })
+//   const axios = require('axios')
+//   const xml = await axios.get('https://rss.allabout.co.jp/aa/latest/ch/health/').then((res) => { return xml = res.data })
   return {
     statusCode: 200,
     body: "Hello, RSS"
