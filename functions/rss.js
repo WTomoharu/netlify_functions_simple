683981755ee9684499417408daf2b49f4534d61f
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-25 13:05:22 +0900

    クロスドメイン許可を追記

--- functions/rss.js
+++ functions/rss.js
@@ -4,6 +4,7 @@ const url = 'https://rss.allabout.co.jp/aa/latest/ch/health/'
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
     body: await getPage(url)
   }
 }
