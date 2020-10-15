019969caaabb8ae22f0bfe3479d30f27fd85b222
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 06:12:30 +0900

    pagesのエラーをさらに出力

--- functions/getPages.js
+++ functions/getPages.js
@@ -4,10 +4,10 @@ exports.handler = async (event, context) => {
 
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
   const pages = await urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => "err"))
-  console.log(JSON.stringify({event, urls, pages}))
+  console.log(JSON.stringify({event, urls, pages, typeof: typeof pages[0]}))
 
   return {
     statusCode: 200,
-    body: `${JSON.stringify({event, urls, pages})}`
+    body: `${JSON.stringify({event, urls, pages, typeof: typeof pages[0]})}`
   }
 }
