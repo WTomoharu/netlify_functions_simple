ca3938d9029ca0752c9f9f00bfdcad4f7b7afbc6
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 06:00:14 +0900

    ログ出力をするように変更

--- functions/getPages.js
+++ functions/getPages.js
@@ -3,7 +3,9 @@ const axios = require('axios')
 exports.handler = async (event, context) => {
 
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
-  const pages = await urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => ""))
+  console.log(JSON.stringify(urls))
+  const pages = await urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => "err"))
+  console.log(JSON.stringify(pages))
 
   return {
     statusCode: 200,
