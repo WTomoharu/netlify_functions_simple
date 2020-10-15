0fa5a3e3dad5300e4d36f7cdd66238505bedd298
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 05:33:52 +0900

    eventも返すように変更

--- functions/getPages.js
+++ functions/getPages.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: `JSON:${JSON.stringify(["HelloGetPages", context])}`
+    body: `${JSON.stringify({context, event})}`
   }
 }
