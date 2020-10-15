fbb2887ad4191fc5f1436babd93cea3888fcdf2e
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 06:22:31 +0900

    Promisがしっかり終わるように、Promise.allを追記

--- functions/getPages.js
+++ functions/getPages.js
@@ -3,11 +3,11 @@ const axios = require('axios')
 exports.handler = async (event, context) => {
 
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
-  const pages = await urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => "err"))
-  console.log(JSON.stringify({event, urls, pages, typeof: typeof pages[0]}))
+  const pages = await Promise.all(urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => "")))
+  console.log(JSON.stringify({event, urls, pages}))
 
   return {
     statusCode: 200,
-    body: `${JSON.stringify({event, urls, pages, typeof: typeof pages[0]})}`
+    body: `${JSON.stringify({event, urls, pages})}`
   }
 }
