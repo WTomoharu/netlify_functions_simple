fd423a5f944f5383ca5808d4aefbf58102f63b1a
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 05:51:22 +0900

    実際のページ取得を実装

--- functions/getPages.js
+++ functions/getPages.js
@@ -1,6 +1,10 @@
 exports.handler = async (event, context) => {
+
+  const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
+  const pages = await urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => ""))
+
   return {
     statusCode: 200,
-    body: `${JSON.stringify({context, event})}`
+    body: `${JSON.stringify(pages)}`
   }
 }
