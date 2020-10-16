a10660e01fa2899057e86e8793cc11dee5e49380
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 09:53:44 +0900

    一時的に配列で返すように変更

--- functions/getPages.js
+++ functions/getPages.js
@@ -3,7 +3,8 @@ const axios = require('axios')
 exports.handler = async (event, context) => {
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
   const pages = await Promise.all(urls.map(async (url) => [url, axios.get(url).then((res) => res.data).catch(() => "")]))
-  const obj = { urls, pages: Object.assign(...pages.map(([k, v]) => ({ [k]: v }))) }
+  // const obj = { urls, pages: Object.assign(...pages.map(([k, v]) => ({ [k]: v }))) }
+  const obj = { urls, pages }
 
   return {
     statusCode: 200,
