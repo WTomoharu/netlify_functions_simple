e41fbf8314c8899b02e608ac959c6e5ddcef37dc
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-17 02:58:41 +0900

    getStationsを実装

---
+++ functions/getStations.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+const myModule = require('./modules/htmlToTime.js')
+
+exports.handler = async (event, context) => {
+  const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
+  const pages = await Promise.all(urls.map(async (url) => axios.get(url).then((res) => myModule(res.data)).catch(() => "")))
+  const obj = { urls, pages: Object.assign(...pages.map((v, i) => ({ [urls[i]]: v }))) }
+
+  return {
+    statusCode: 200,
+    body: `${JSON.stringify(obj)}`,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  }
+}
---
+++ functions/modules/htmlToTime.js
@@ -0,0 +1,57 @@
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+let Dict;
+
+function getDict(dom) {
+  const trainType = {}
+  for (const item of Array.from(dom.querySelectorAll("#timeNotice1 td ul li"))) {
+    const [key, value] = item.textContent.split("：")
+    trainType[key] = value
+  }
+
+  const trainFor = {}
+  for (const item of Array.from(dom.querySelectorAll("#timeNotice2 td ul li"))) {
+    const [key, value] = item.textContent.split("：")
+    trainFor[key] = value
+  }
+
+  return { trainType, trainFor }
+}
+
+class Cell {
+  constructor(li, hour) {
+    this.hour = hour
+    this.minute = li.querySelector("dt").textContent
+
+    const trainTypeKey = li.querySelector(".trainType") ?
+      li.querySelector(".trainType").textContent.slice(1, -1) : "無印"
+    this.trainType = Dict.trainType[trainTypeKey]
+
+    const trainForKey = li.querySelector(".trainFor") ?
+      li.querySelector(".trainFor").textContent : "無印"
+    this.trainFor = Dict.trainFor[trainForKey]
+
+    this.url = li.querySelector("a").href
+  }
+}
+
+class Row {
+  constructor(tr) {
+    const lis = Array.from(tr.querySelectorAll(".timeNumb"))
+    const hour = tr.querySelector(".hour").textContent
+    this.cells = lis.map(li => new Cell(li, hour))
+  }
+}
+
+function main(jsonText) {
+  const jsonData = JSON.parse(jsonText)
+  const dom = new JSDOM(jsonData.pages[jsonData.urls[0]]).window.document
+  Dict = getDict(dom)
+  const trs = Array.from(dom.querySelectorAll('.tblDiaDetail tr')).slice(1)
+  const rows = trs.map(tr => new Row(tr))
+  const out = [].concat(...rows.map(row => row.cells))
+  return JSON.stringify(out)
+}
+
+module.exports = main
