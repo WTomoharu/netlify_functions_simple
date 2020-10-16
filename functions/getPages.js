99ede67635912db85b007390102ba9b1bb858cf5
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 09:56:47 +0900

    obj生成周りのバグを修正

--- functions/getPages.js
+++ functions/getPages.js
@@ -2,9 +2,8 @@ const axios = require('axios')
 
 exports.handler = async (event, context) => {
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
-  const pages = await Promise.all(urls.map(async (url) => [url, axios.get(url).then((res) => res.data).catch(() => "")]))
-  // const obj = { urls, pages: Object.assign(...pages.map(([k, v]) => ({ [k]: v }))) }
-  const obj = { urls, pages }
+  const pages = await Promise.all(urls.map(async (url) => axios.get(url).then((res) => res.data).catch(() => "")))
+  const obj = { urls, pages: Object.assign(...pages.map((v, i) => ({ [urls[i]]: v }))) }
 
   return {
     statusCode: 200,
