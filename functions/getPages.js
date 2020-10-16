09d5cf671c9a03b5b54cd512061f48cfd280df89
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-16 09:49:33 +0900

    返すデータ形式を整理

--- functions/getPages.js
+++ functions/getPages.js
@@ -1,13 +1,13 @@
 const axios = require('axios')
 
 exports.handler = async (event, context) => {
-
   const urls = (event.queryStringParameters.urls || "").split(",").filter(v => v)
-  const pages = await Promise.all(urls.map(async (url) => axios.get(url).then((res) => res.data ).catch(() => "")))
-  console.log(JSON.stringify({event, urls, pages}))
+  const pages = await Promise.all(urls.map(async (url) => [url, axios.get(url).then((res) => res.data).catch(() => "")]))
+  const obj = { urls, pages: Object.assign(...pages.map(([k, v]) => ({ [k]: v }))) }
 
   return {
     statusCode: 200,
-    body: `${JSON.stringify({event, urls, pages})}`
+    body: `${JSON.stringify(obj)}`,
+    headers: { 'Access-Control-Allow-Origin': '*' },
   }
 }
